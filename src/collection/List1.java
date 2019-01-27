6e20975b063a7afddabf58095acb8b6ac468d4b5
김동식 <dongsik2026@naver.com>
2019-01-27 22:55:58 +0900

    init

---
+++ src/collection/List1.java
@@ -0,0 +1,25 @@
+package collection;
+
+public class List1 {
+	private Object[] elements = new Object[10];
+	private boolean readOnly;
+	private int size = 0;
+
+	public void add(Object element) {
+		if (!readOnly) {
+			int newSize = size + 1;
+			
+			if ( newSize > elements.length) {
+				Object[] newElements = new Object[elements.length + 10];
+				for (int i = 0; i < size; i++) {
+					newElements[i] = elements[i];
+				}
+
+				elements = newElements;
+			}
+
+			elements[size] = element;
+			size++;
+		}
+	}
+}
