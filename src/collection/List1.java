fbbaea4bbe7d0be657e3f726aba0d5782194bf7e
kyungjukang <47440694+kyungjukang@users.noreply.github.com>
2019-02-11 11:03:10 +0900

    상수처리
    STORE_SIZE_INCREMENT =10 상수처리됨

--- src/collection/List1.java
+++ src/collection/List1.java
@@ -1,6 +1,7 @@
 package collection;
 
 public class List1 {
+	private static final int STORE_SIZE_INCREMENT = 10;
 	private Object[] elements = new Object[10];
 	private boolean readOnly;
 	private int size = 0;
@@ -10,7 +11,7 @@ public class List1 {
 			int newSize = size + 1;
 			
 			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
+				Object[] newElements = new Object[elements.length + STORE_SIZE_INCREMENT];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
 				}
