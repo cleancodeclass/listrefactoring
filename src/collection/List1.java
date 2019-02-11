30203d183808492ef83931df7807c9a5c856099e
seunghyeun <dltmdgus0426@naver.com>
2019-02-11 11:24:09 +0900

    C_IF문 부정형을 긍정형으로 변경
    !readOnly 부분을 readOnly False 인경우 return 으로 빠져 나가게 바꾸었습니다.

--- src/collection/List1.java
+++ src/collection/List1.java
@@ -1,26 +1,26 @@
 package collection;
 
 public class List1 {
+	
 	private static final int STORE_SIZE_INCREMENT = 10;
 	private Object[] elements = new Object[10];
 	private boolean readOnly;
 	private int size = 0;
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			
-			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + STORE_SIZE_INCREMENT];
-				for (int i = 0; i < size; i++) {
-					newElements[i] = elements[i];
-				}
-
-				elements = newElements;
+		if (readOnly) return;
+		
+		int newSize = size + 1;
+		
+		if ( newSize > elements.length) {
+			Object[] newElements = new Object[elements.length + STORE_SIZE_INCREMENT];
+			for (int i = 0; i < size; i++) {
+				newElements[i] = elements[i];
 			}
 
-			elements[size] = element;
-			size++;
+			elements = newElements;
 		}
+		elements[size] = element;
+		size++;
 	}
 }
