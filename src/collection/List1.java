2395422be189d22a23ed9ce0f2067928c33a6393
ldh7262-lee <50975082+ldh7262-lee@users.noreply.github.com>
2019-05-27 17:23:55 +0900

    Update List1.java
    Magic Number Refactoring

--- src/collection/List1.java
+++ src/collection/List1.java
@@ -1,7 +1,10 @@
 package collection;
 
 public class List1 {
-	private Object[] elements = new Object[10];
+	private static final int INCREMENT_STORE_SIZE = 10;
+	private static final int INITAL_STORE_SIZE = 10;
+	
+	private Object[] elements = new Object[INITAL_STORE_SIZE];
 	private boolean readOnly;
 	private int size = 0;
 
@@ -10,7 +13,7 @@ public class List1 {
 			int newSize = size + 1;
 			
 			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
+				Object[] newElements = new Object[elements.length + INCREMENT_STORE_SIZE];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
 				}
