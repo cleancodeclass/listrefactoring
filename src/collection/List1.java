c0d3573dab402e88bb8a5621908f2cf80c382330
mo21kim <mulkyun@gmail.com>
2019-05-27 17:24:33 +0900

    magic number
    magic number

--- src/collection/List1.java
+++ src/collection/List1.java
@@ -1,25 +1,38 @@
 package collection;
 
 public class List1 {
-	private Object[] elements = new Object[10];
+	private static final int INCREMENT_SIZE = 1;
+	private static final int INCREMENT_STORE_SIZE = 10;
+	private static final int INITIAL_STORE_SIZE = 10;
+	private Object[] elements = new Object[INITIAL_STORE_SIZE];
 	private boolean readOnly;
 	private int size = 0;
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			
-			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
-				for (int i = 0; i < size; i++) {
-					newElements[i] = elements[i];
-				}
-
-				elements = newElements;
-			}
-
-			elements[size] = element;
-			size++;
+		if (readOnly) {
+			return;
 		}
+		
+		if ( isStoreFull(size + INCREMENT_SIZE)) {
+			incrementStoreSize();
+		}
+		addElementAtEnd(element);
+	}
+
+	private void addElementAtEnd(Object element) {
+		elements[size] = element;
+		size++;
+	}
+
+	private void incrementStoreSize() {
+		Object[] newElements = new Object[elements.length + INCREMENT_STORE_SIZE];
+		for (int i = 0; i < size; i++) {
+			newElements[i] = elements[i];
+		}
+		elements = newElements;
+	}
+
+	private boolean isStoreFull(int newSize) {
+		return newSize > elements.length;
 	}
 }
