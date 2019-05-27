cb8cc9b02931a0f984fa32dbf5e0a9749c5b3178
nubang7 <42139382+nubang7@users.noreply.github.com>
2019-05-27 10:55:17 +0900

    Update List1.java

--- src/collection/List1.java
+++ src/collection/List1.java
@@ -1,8 +1,7 @@
 package collection;
 
 public class List1 {
-	private static final int STORE_SIZE_INCREMENT = 10;
-	private Object[] elements = new Object[STORE_SIZE_INCREMENT];
+	private Object[] elements = new Object[10];
 	private boolean readOnly;
 	private int size = 0;
 
@@ -11,7 +10,7 @@ public class List1 {
 			int newSize = size + 1;
 			
 			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + STORE_SIZE_INCREMENT];
+				Object[] newElements = new Object[elements.length + 10];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
 				}
