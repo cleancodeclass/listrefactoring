b10b6158ae029a1123f73dacbb327b67803b1573
mjhur <mjhur@users.noreply.github.com>
2019-05-27 17:23:35 +0900

    Convert IF statment
    Convert IF statment

--- src/collection/List1.java
+++ src/collection/List1.java
@@ -6,10 +6,14 @@ public class List1 {
 	private int size = 0;
 
 	public void add(Object element) {
-		if (!readOnly) {
+		if (readOnly) {
+			return;
+		}
+
+		else {
 			int newSize = size + 1;
-			
-			if ( newSize > elements.length) {
+
+			if (newSize > elements.length) {
 				Object[] newElements = new Object[elements.length + 10];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
