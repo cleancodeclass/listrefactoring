92ad326af6c3fa484531af822c4bcf1f9796cc5c
aeisee <51064308+aeisee@users.noreply.github.com>
2019-05-27 17:23:38 +0900

    update List1.java
    add comment

--- src/collection/List1.java
+++ src/collection/List1.java
@@ -1,25 +1,39 @@
 package collection;
 
 public class List1 {
-	private Object[] elements = new Object[10];
+	private static final int INCREMENT_SIZE = 10; // rename
+	private static final int INITIAL_SIZE = 10; // rename
+	private Object[] elements = new Object[INITIAL_SIZE];
 	private boolean readOnly;
 	private int size = 0;
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			
-			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
-				for (int i = 0; i < size; i++) {
-					newElements[i] = elements[i];
-				}
-
-				elements = newElements;
+		if (readOnly) {
+			return;
+		} else { // add else block
+			if (isStoreFull(size + 1)) {
+				incrementElementsSize();
 			}
 
-			elements[size] = element;
-			size++;
+			addElementAtEnd(element);
 		}
+	}
+
+	private void addElementAtEnd(Object element) {
+		elements[size] = element;
+		size++;
+	}
+
+	private void incrementElementsSize() {
+		Object[] newElements = new Object[elements.length + INCREMENT_SIZE];
+		for (int i = 0; i < size; i++) {
+			newElements[i] = elements[i];
+		}
+
+		elements = newElements;
+	}
+
+	private boolean isStoreFull(int size) { // rename
+		return size > elements.length;
 	}
 }
