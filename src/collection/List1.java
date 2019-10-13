e865adc57784e8fe5fca9543db26ee35ec65a0fd
Pablo <dongsik2026@naver.com>
2019-10-14 00:40:24 +0900

    Update List1.java

--- src/collection/List1.java
+++ src/collection/List1.java
@@ -1,28 +1,25 @@
 package collection;
 
-
 public class List1 {
 	private Object[] elements = new Object[10];
 	private boolean readOnly;
 	private int size = 0;
 
 	public void add(Object element) {
-		if (readOnly) {
-			return;
-		}
-		
-		int newSize = size + 1;
+		if (!readOnly) {
+			int newSize = size + 1;
+			
+			if ( newSize > elements.length) {
+				Object[] newElements = new Object[elements.length + 10];
+				for (int i = 0; i < size; i++) {
+					newElements[i] = elements[i];
+				}
 
-		if ( newSize > elements.length) {
-			Object[] newElements = new Object[elements.length + 10];
-			for (int i = 0; i < size; i++) {
-				newElements[i] = elements[i];
+				elements = newElements;
 			}
 
-			elements = newElements;
+			elements[size] = element;
+			size++;
 		}
-
-		elements[size] = element;
-		size++;
 	}
 }
